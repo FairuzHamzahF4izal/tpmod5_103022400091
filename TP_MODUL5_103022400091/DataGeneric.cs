83ff0022cf0f8cba0f9a6096193531e1a1d9f0fc
FairuzHamzahF4izal <fairuzhamzah1@gmail.com>
2026-03-29 20:14:17 +0800

    tambah class DataGeneric

---
+++ TP_MODUL5_103022400091/DataGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class DataGeneric<T>
+{
+    public T Data;
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
--- TP_MODUL5_103022400091/Program.cs
+++ TP_MODUL5_103022400091/Program.cs
@@ -1,1 +1,2 @@
-﻿Console.WriteLine("Hello, World!");
+﻿DataGeneric<string> data = new DataGeneric<string>("Fairuz");
+data.PrintData();
